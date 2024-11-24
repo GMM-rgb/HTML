11bb11f6fd256c8d93619936b12b25be2faf5f1c
GMM-rgb <maximus.farvour@outlook.com>
2024-11-24 15:23:45 -0800

    Add files via upload

---
+++ .github/SearchProcces1.js
@@ -0,0 +1,17 @@
+﻿function searchFiles() {
+            const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+            const files = [
+                { name: "Search_Pages/page1.html", displayName: "Board Customization" },
+                { name: "Search_Pages/page2.html", displayName: "About HTML Searcher" },
+                { name: "Language_Arts_Istamgram_Project/Language_Arts_Istamgram_Project_SlideManager.html", displayName: "Language Arts Instagram Project" },
+                //Files URLMappings
+            ];
+
+
+            const result = files.filter(file => file.displayName.toLowerCase() === searchTerm);
+
+
+            // Display result in iframe
+            const iframe = document.getElementById("resultFrame");
+            iframe.src = result.length > 0 ? result[0].name : "about:blank";
+        }
